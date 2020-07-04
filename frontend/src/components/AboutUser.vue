f0e9f5abd3313ad3941cd28783823a0df7939a46
Vladislav Ponomarev <farazeus@gmail.com>
2020-07-04 16:28:31 +0300

    WIP: Never before had a small typo like this one caused so much damage.

--- frontend/src/components/AboutUser.vue
+++ frontend/src/components/AboutUser.vue
@@ -2,6 +2,7 @@
   <div>
     <AppHeader></AppHeader>
     <h3>About User</h3>
+    Your email is {{ user.email }} and you are {{ user.role }} user.
   </div>
 </template>
 
@@ -10,10 +11,24 @@ import AppHeader from '@/components/AppHeader'
 
 export default {
   name: 'AboutUser',
+  data () {
+    return {
+      user: {}
+    }
+  },
   created () {
     if (!this.$store.state.signedIn) {
       this.$router.replace('/')
+    } else {
+      this.$http.secured.get('/api/users/whoami')
+        .then(response => { this.user = response.data })
+        .catch(error => { this.setError(error, 'Something went wrong') })
     }
+  },
+  methods: {
+    setError (error, text) {
+      this.error = (error.response && error.response.data && error.response.data.error) || text
+    },
   },
   components: { AppHeader }
 }
