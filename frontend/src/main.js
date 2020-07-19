0c7c9686747aa5d31d55223044043b4e12ba3144
Vladislav Ponomarev <farazeus@gmail.com>
2020-07-19 23:22:37 +0300

    Add vue-i18n + some related changes

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,6 +4,7 @@ import '@lib/registerServiceWorker'
 import router from '@router'
 import { store } from '@store'
 import '@plugins/element.js'
+import i18n from '@lib/i18n'
 
 Vue.config.productionTip = false
 
@@ -11,5 +12,6 @@ new Vue({
   el: '#app',
   router,
   store,
+  i18n,
   render: h => h(App)
 }).$mount('#app')
