920f5d4f768315f297888dc5a9a245a581bc1aa0
Vladislav Ponomarev <farazeus@gmail.com>
2020-07-20 01:53:50 +0300

    WIP: another big bag of changes

--- frontend/src/store/modules/user/actions.js
+++ frontend/src/store/modules/user/actions.js
@@ -1,14 +1,12 @@
-// import Vue from 'vue'
 import messageToast from '@lib/messageToast'
-import * as types from './mutation_types'
 
 export const unsetCurrentUser = ({ commit }, { errorMessage }) => {
-  commit(types.UNSET_CURRENT_USER)
+  commit('UNSET_CURRENT_USER')
   if (typeof (errorMessage) !== 'undefined') {
     messageToast.showError(errorMessage)
   }
 }
 
 export const setCurrentUser = ({ commit }, { currentUser, csrf }) => {
-  commit(types.SET_CURRENT_USER, { currentUser: currentUser, csrf: csrf })
+  commit('SET_CURRENT_USER', { currentUser: currentUser, csrf: csrf })
 }
--- frontend/src/store/modules/user/mutations.js
+++ frontend/src/store/modules/user/mutations.js
@@ -1,12 +1,10 @@
-import * as types from './mutation_types'
-
 export default {
-  [types.SET_CURRENT_USER] (state, { currentUser, csrf }) {
+  SET_CURRENT_USER: (state, { currentUser, csrf }) => {
     state.currentUser = currentUser
     state.signedIn = true
     state.csrf = csrf
   },
-  [types.UNSET_CURRENT_USER] (state) {
+  UNSET_CURRENT_USER: (state) => {
     state.currentUser = {}
     state.signedIn = false
     state.csrf = null
